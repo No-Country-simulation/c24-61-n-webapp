966890ec3591f6e59dc1cd4bb93f4a22256ccd3d
fsalasevicius <flaviosalasevicius@hotmail.com>
2025-03-06 17:55:14 -0300

    generete home page

--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeHeroComponent } from '../../components/home/home-hero/home-hero.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HomeHeroComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
