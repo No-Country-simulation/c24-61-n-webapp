f5c8592d06065a0aa9209146519a4deb5fca8215
fsalasevicius <flaviosalasevicius@hotmail.com>
2025-03-06 18:17:32 -0300

    fix routerLink navbar

--- front/src/app/components/navbar/navbar.component.ts
+++ front/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
