d09cfd0cb6240292bf97a128127d6bd6319c6021
Flavio Salasevicius <70928392+fsalasevicius@users.noreply.github.com>
2025-03-08 08:50:38 -0300

    cambios

--- front/src/app/auth/login/login.component.ts
+++ front/src/app/auth/login/login.component.ts
@@ -1,56 +1,82 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
-import {Toast } from 'primeng/toast';
-import { AuthService } from '../services/auth.services';
+import { Toast } from 'primeng/toast';
+import { AuthService } from '../../core/services/auth.services';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterLink, ButtonModule, Toast],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterLink, ButtonModule, Toast],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   providers: [MessageService]
 })
 export class LoginComponent {
-  username = signal('');
-  password = signal('');
+  loginForm: FormGroup;
   showPassword: boolean = false;
   errorMessage: string = '';
 
-  constructor(private _router: Router, private _messageService:MessageService, private _authService:AuthService) {}
+  constructor(
+    private _fb: FormBuilder,
+    private _router: Router,
+    private _messageService: MessageService,
+    private _authService: AuthService
+  ) {
+    console.log('FormBuilder:', this._fb);
+
+    this.loginForm = this._fb?.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
+
 
   togglePassword() {
     this.showPassword = !this.showPassword;
   }
 
-  login(form: NgForm) {
-    if (form.invalid) {
-      this.errorMessage = 'Por favor, complete todos los campos correctamente.';
-      return;
+  ngOnInit(): void {
+    console.log('Verificando autenticación...');
+    const userLoggedIn = this._authService.isAuthenticated();
+    console.log('Usuario autenticado:', userLoggedIn);
+
+    if (userLoggedIn) {
+      this._router.navigate(['/home']);
     }
-
-    this._authService.login(this.username(), this.password()).subscribe({
-      next: (response) => {
-        this._messageService.add({
-          severity: 'success',
-          summary: 'Operación Exitosa',
-          detail: 'Redireccionando al Home.'
-        });
-        this._router.navigate(['/dashboard']);
-      },
-      error: () => {
-        this.errorMessage = 'Credenciales incorrectas.';
-      }
-    });
   }
 
-  show(){
-    this._messageService.add({severity:'info', summary: 'Info', detail: 'Ha iniciado sesión correctamente.'});
+  onSubmit(): void {
+    console.log('Formulario enviado:', this.loginForm.value);
+
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      this._authService.login(email, password).subscribe(
+        (response) => {
+          console.log('Respuesta de login:', response);
+          const onboardingComplete = response.onboardingComplete ?? false;
+          this._authService.saveSession(response.jwt);
+
+          this._messageService.add({ severity: 'success', summary: 'Operación Exitosa', detail: 'Ingresando a la plataforma.' });
+
+          setTimeout(() => {
+            this._router.navigate([onboardingComplete ? '/home' : '/login']);
+          }, 1000);
+        },
+        (error) => {
+          console.error('Error en el login:', error);
+          this._messageService.add({ severity: 'error', summary: 'Error', detail: 'Error en el login. Verifica tus credenciales.' });
+        }
+      );
+    } else {
+      console.warn('El formulario no es válido:', this.loginForm);
+    }
   }
 
+  show() {
+    this._messageService.add({ severity: 'info', summary: 'Info', detail: 'Ha iniciado sesión correctamente.' });
+  }
 }
-
